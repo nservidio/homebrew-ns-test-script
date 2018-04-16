13e835ef5de65e0865d59fc61f3953167c573e08
Nick Servidio <servidionicholas@gmail.com>
2018-04-15 23:18:18 -0400

    updated dependency

--- test_script.rb
+++ test_script.rb
@@ -1,9 +1,11 @@
 class TestScript < Formula
   desc "A command line tool"
   homepage "https://github.com/TheHipbot/weather"
-  url "https://github.com/nservidio/test-script/archive/test_script-1.0.0.tar.gz"
+  url "https://github.com/nservidio/test-script/raw/archive/test_script-1.0.0.tar.gz"
   sha256 "1756ce80a28b818fea5ddc94f041ca4b3e43029696c81acf074faf0450849aa7"
   version "1.0.0"
+
+  depends_on "curl"
 
   bottle :unneeded
 
