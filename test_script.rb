9cda1dfcc7ae318795550e054268fdf53c456e4c
Nick Servidio <servidionicholas@gmail.com>
2018-04-15 23:04:06 -0400

    added a file

---
+++ test_script.rb
@@ -0,0 +1,13 @@
+class Weather < Formula
+  desc "A command line tool"
+  homepage "https://github.com/TheHipbot/weather"
+  url "https://github.com/nservidio/test-script/blob/master/archive/test_script-1.0.0.tar.gz?raw=true"
+  sha256 "1756ce80a28b818fea5ddc94f041ca4b3e43029696c81acf074faf0450849aa7"
+  version "1.0.0"
+
+  bottle :unneeded
+
+  def install
+    bin.install "weather"
+  end
+end
